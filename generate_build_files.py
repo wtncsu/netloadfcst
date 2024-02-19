3b8f1a542339a2c72b9c85c53b26e5281276de6a
John Yu <byu9@ncsu.edu>
2024-02-19 18:40:52 -0500

    add more build targets

--- generate_build_files.py
+++ generate_build_files.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from glob import glob
 from pathlib import Path
 
@@ -5,43 +7,52 @@ from ninja_syntax import Writer
 
 writer = Writer(open('build.ninja', 'w'))
 
-netload_only_settings = [
-    Path(file)
-    for file in glob('settings/netload_only/*.toml')
-]
 
-for file in netload_only_settings:
-    config = f'{file}'
-    train_feature = f'netload_only_train/feature-{file.stem}.csv'
-    train_target = f'netload_only_train/target-{file.stem}.csv'
-    test_feature = f'netload_only_test/feature-{file.stem}.csv'
-    predict_mean = f'output/netload_only/mean-{file.stem}.csv'
-    predict_std = f'output/netload_only/std-{file.stem}.csv'
-    visualize_tree = f'output/netload_only/tree-{file.stem}.svg'
-    train_time = f'output/netload_only/train-time-{file.stem}'
-    test_time = f'output/netload_only/test-time-{file.stem}'
+def generate(category):
+    settings = [
+        Path(file)
+        for file in glob(f'settings/{category}/*.toml')
+    ]
 
-    rule_name = f'netload_only-{file.stem}'
+    for file in settings:
+        config = f'{file}'
+        train_feature = f'{category}_train/feature-{file.stem}.csv'
+        train_target = f'{category}_train/target-{file.stem}.csv'
+        test_feature = f'{category}_test/feature-{file.stem}.csv'
+        predict_mean = f'output/{category}/mean-{file.stem}.csv'
+        predict_std = f'output/{category}/std-{file.stem}.csv'
+        visualize_tree = f'output/{category}/tree-{file.stem}.svg'
+        train_time = f'output/{category}/train-time-{file.stem}'
+        test_time = f'output/{category}/test-time-{file.stem}'
 
-    command = (
-        './run_model.py '
-        f'--config={config} '
-        f'--train-feature={train_feature} '
-        f'--train-target={train_target} '
-        f'--test-feature={test_feature} '
-        f'--predict-mean={predict_mean} '
-        f'--predict-std={predict_std} '
-        f'--visualize-tree={visualize_tree} '
-        f'--train-time={train_time} '
-        f'--test-time={test_time} '
-    )
+        rule_name = f'{category}-{file.stem}'
 
-    writer.rule(name=rule_name, command=command)
-    writer.newline()
+        command = (
+            './run_model.py '
+            f'--config={config} '
+            f'--train-feature={train_feature} '
+            f'--train-target={train_target} '
+            f'--test-feature={test_feature} '
+            f'--predict-mean={predict_mean} '
+            f'--predict-std={predict_std} '
+            f'--visualize-tree={visualize_tree} '
+            f'--train-time={train_time} '
+            f'--test-time={test_time} '
+        )
 
-    writer.build(
-        rule=rule_name,
-        outputs=[predict_mean, predict_std, visualize_tree],
-        inputs=[train_feature, train_target, test_feature]
-    )
-    writer.newline()
+        writer.rule(name=rule_name, command=command)
+        writer.newline()
+
+        writer.build(
+            rule=rule_name,
+            outputs=[predict_mean, predict_std, visualize_tree],
+            inputs=[train_feature, train_target, test_feature]
+        )
+        writer.newline()
+
+
+generate('netload_only')
+generate('potential_only')
+generate('netload')
+generate('potential')
+generate('combined')
