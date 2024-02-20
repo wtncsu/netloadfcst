22e1e16e587f6a88da3ce1a3919ad87401477ec8
John Yu <byu9@ncsu.edu>
2024-02-19 22:43:13 -0500

    support multiple targets

--- plot_prediction.py
+++ plot_prediction.py
@@ -13,79 +13,77 @@ parser = ArgumentParser()
 parser.add_argument('--mean', type=Path, required=True)
 parser.add_argument('--std', type=Path, required=True)
 parser.add_argument('--target', type=Path)
-parser.add_argument('--save', type=Path)
 args = parser.parse_args()
 
 mean = read_csv(args.mean, index_col=0)
 std = read_csv(args.std, index_col=0)
 target = (read_csv(args.target, index_col=0)
           if args.target is not None else None)
-dist = norm(loc=mean, scale=std)
 
-title = (
-    f'Pred mean={args.mean}\nstd={args.std}\n'
-    f'Target={args.target}'
-)
-
-band_prob_lo = 0.05
-band_prob_hi = 0.95
-
-band_lo = dist.ppf(band_prob_lo)
-band_hi = dist.ppf(band_prob_hi)
-
-if args.target is not None:
-    outcome_lo = np.min((target, band_lo))
-    outcome_hi = np.max((target, band_hi))
-else:
-    outcome_lo = target.min()
-    outcome_hi = target.max()
-
-resolution = 500
-outcomes = np.linspace(outcome_lo, outcome_hi, resolution)
-outcome_probs = dist.pdf(outcomes).transpose()
-outcome_probs /= outcome_probs.max()
 datetime = date2num(mean.index)
-date_format = DateFormatter('%b %-d %H:%M')
 
-figure = plt.figure(figsize=(12, 4))
 
-ax1 = plt.subplot(121)
-extent = (datetime.min(), datetime.max(), outcome_lo, outcome_hi)
-heatmap = plt.imshow(outcome_probs, extent=extent, aspect='auto',
-                     origin='lower', cmap='hot')
-ax1.xaxis.set_major_formatter(date_format)
-plt.grid(color='white', alpha=0.1)
-plt.colorbar(label='Probability density')
+def annotate_plot(title):
+    date_format = DateFormatter('%b %-d %H:%M')
+    plt.gca().xaxis.set_major_formatter(date_format)
 
-if target is not None:
-    plt.scatter(datetime, target, color='white', marker='+', s=12,
-                linewidths=0.5, label='Observations', alpha=0.9)
-plt.legend()
-plt.xlabel('Time')
-plt.ylabel('Outcome')
-plt.xticks(rotation=45)
-plt.title(title)
+    plt.xlabel('Time')
+    plt.ylabel('Outcome')
+    plt.xticks(rotation=15)
+    plt.title(title)
 
-ax2 = plt.subplot(122, sharex=ax1, sharey=ax1)
 
-if target is not None:
-    plt.plot(datetime, target, marker='+', label='Observations')
+def plot_heatmap(dist, target=None, q_low=1e-3, q_high=1 - 1e-3,
+                 resolution=500):
+    outcome_lo = dist.ppf(q_low).min()
+    outcome_hi = dist.ppf(q_high).max()
+    outcomes = np.linspace(outcome_lo, outcome_hi, resolution).reshape(-1, 1)
+    density = dist.pdf(outcomes)
 
-plt.plot(datetime, mean, label='Prediction mean', marker='+')
-plt.fill_between(datetime, band_lo.ravel(), band_hi.ravel(),
-                 color='black', alpha=0.2,
-                 label=f'Prediction band {band_prob_lo} ~ {band_prob_hi}')
-ax2.xaxis.set_major_formatter(date_format)
-plt.grid()
-plt.legend()
-plt.xlabel('Time')
-plt.ylabel('Outcome')
-plt.xticks(rotation=45)
-plt.title(title)
+    plt.imshow(density, origin='lower', cmap='hot', aspect='auto',
+               extent=(datetime.min(), datetime.max(), outcome_lo, outcome_hi))
 
-if args.save is not None:
-    args.save.parent.mkdir(parents=True, exist_ok=True)
-    plt.savefig(args.save, dpi=300, format='png')
+    plt.grid(color='white', alpha=0.1)
+    plt.colorbar(label='Prob. density')
 
-else:
-    plt.show()
+    if target is not None:
+        plt.scatter(datetime, target, s=1, label='Observations', alpha=0.4)
+        plt.legend()
+
+
+def plot_interval(dist, target=None, q_low=0.05, q_high=0.95):
+    band_lo = dist.ppf(q_low)
+    band_hi = dist.ppf(q_high)
+
+    if target is not None:
+        plt.plot(datetime, target, marker='+', label='Observations')
+
+    plt.plot(datetime, dist.mean(), label='Pred. mean')
+    plt.fill_between(datetime, band_lo.ravel(), band_hi.ravel(), color='black',
+                     alpha=0.2, label=f'Pred. band {q_low} ~ {q_high}')
+
+    plt.legend()
+    plt.grid()
+
+
+for col in mean.columns:
+    title = (
+        f'Pred mean={args.mean}\nstd={args.std}\n'
+        f'Target={args.target}:{col}'
+    )
+
+    dist = norm(mean[col], std[col])
+
+    plt.figure(figsize=(12, 4))
+
+    ax1 = plt.subplot(121)
+    plot_heatmap(dist, target=target[col])
+    annotate_plot(title)
+
+    plt.subplot(122, sharex=ax1, sharey=ax1)
+    plot_interval(dist, target=target[col])
+    annotate_plot(title)
+
+    plt.subplots_adjust(left=0.07, right=0.98, top=0.8, bottom=0.19)
+
+plt.show()
